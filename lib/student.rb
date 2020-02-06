20d6a108adada2536bde642de30b66bbdd2b7524
iiakar <ibsakar>
2020-02-06 18:18:18 -0500

    Done.

---
+++ lib/student.rb
@@ -0,0 +1,17 @@
+class Student < User
+    attr_accessor :first_name, :last_name
+
+    def initialize
+      @knowledge = []
+    end
+
+    def learn(str)
+      @knowledge << str
+    end
+
+    def knowledge
+      @knowledge
+    end 
+
+
+end
